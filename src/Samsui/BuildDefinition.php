84e7f756c02f983e1219dd07641ae1b3451fcabd
Sam-Mauris Yong <mauris@hotmail.sg>
2013-10-31 22:16:28 +0800

    build definition concrete implementation

---
+++ src/Samsui/BuildDefinition.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Samsui;
+
+class BuildDefinition implements BuildDefinitionInterface
+{
+    public function sequence($name)
+    {
+    }
+
+    public function attr($name, $value)
+    {
+    }
+}
