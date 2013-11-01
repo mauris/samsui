9c730705a900740cd495d445dff29187a96276c8
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-02 00:06:48 +0800

    test attributes

--- test/Samsui/DefinitionTest.php
+++ test/Samsui/DefinitionTest.php
@@ -25,4 +25,28 @@ class DefinitionTest extends PHPUnit_Framework_TestCase
             $this->assertEquals($i, $obj->number);
         }
     }
+
+    public function testAttributes()
+    {
+        $definition = new Definition();
+        $definition
+            ->attr('age', 10)
+            ->attr('category', function ($i) {
+                return $i;
+            });
+
+        $obj1 = $definition->build();
+        $this->assertTrue(property_exists($obj1, 'age'));
+        $this->assertTrue(property_exists($obj1, 'category'));
+        $this->assertEquals(10, $obj1->age);
+        $this->assertEquals(1, $obj1->category);
+
+        for ($i = 2; $i < 10; ++$i) {
+            $obj = $definition->build();
+            $this->assertTrue(property_exists($obj, 'age'));
+            $this->assertTrue(property_exists($obj, 'category'));
+            $this->assertEquals(10, $obj->age);
+            $this->assertEquals($i, $obj->category);
+        }
+    }
 }
