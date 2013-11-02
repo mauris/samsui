df84ebf3ee2162cb2afd2dde38403faaa6eba25f
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-02 13:43:25 +0800

    added test properties

--- test/Samsui/WrapperTest.php
+++ test/Samsui/WrapperTest.php
@@ -6,4 +6,18 @@ use \PHPUnit_Framework_TestCase;
 
 class WrapperTest extends PHPUnit_Framework_TestCase
 {
+    public function testProperties()
+    {
+        $wrapper = new Wrapper(
+            array('name' => 'John Sim'),
+            array()
+        );
+
+        $this->assertEquals('John Sim', $wrapper->name);
+        $wrapper->name = 'Gummy Bear';
+        $this->assertEquals('Gummy Bear', $wrapper->name);
+        $this->assertTrue(isset($wrapper->name));
+        unset($wrapper->name);
+        $this->assertFalse(isset($wrapper->name));
+    }
 }
