c1741493967d40cfc20d1a68d25bb965f377b557
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-03 09:05:51 +0800

    Factory in-code docs

--- src/Samsui/Factory.php
+++ src/Samsui/Factory.php
@@ -11,6 +11,10 @@ class Factory implements FactoryInterface, ConsumerInterface
 
     protected $objects;
 
+    /**
+     * Create a Factory object
+     * @since 1.0.0
+     */
     public function __construct()
     {
         $this->objects = new ObjectCollection();
@@ -18,6 +22,12 @@ class Factory implements FactoryInterface, ConsumerInterface
         $this->container['Samsui\\DefinitionInterface'] = $this->container->instance('Samsui\\Definition');
     }
 
+    /**
+     * Define a object build definition
+     * @param string $name The identifier of the object to be defined
+     * @return Samsui\DefinitionInterface Returns the Definition for building the object
+     * @since 1.0.0
+     */
     public function define($name)
     {
         if (!$this->objects->get($name)) {
@@ -26,6 +36,13 @@ class Factory implements FactoryInterface, ConsumerInterface
         return $this->objects->get($name);
     }
 
+    /**
+     * Build objects using the build definition
+     * @param string $name The identifier of the build definition
+     * @param int $quantity (optional) The number of objects to build, defaults to 1 only. If the quantity specified is more than 1, an array of objects will be returned instead of just the object.
+     * @return mixed|array Returns the resultant object. If the quantity is more than one, an array of the objects is returned instead.
+     * @since 1.0.0
+     */
     public function build($name, $quantity = 1)
     {
         if ($definition = $this->objects->get($name)) {
@@ -41,6 +58,12 @@ class Factory implements FactoryInterface, ConsumerInterface
         throw new \Exception('Tried to build undefined Samsui definition "' . $name . '".');
     }
 
+    /**
+     * FuelBlade IoC injection of container.
+     * @param Packfire\FuelBlade\Container $container The container for dependencies to be injected.
+     * @since 1.0.0
+     * @internal
+     */
     public function __invoke($container)
     {
         $this->container = $container;
