f34e32a509e1f6d769857518fb927773d95e263d
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-04 11:09:36 +0800

    added tests for randomNumber()

--- test/Samsui/Generator/Provider/MathTest.php
+++ test/Samsui/Generator/Provider/MathTest.php
@@ -9,8 +9,16 @@ class MathnTest extends PHPUnit_Framework_TestCase
     public function testBetween()
     {
         $provider = new Math();
-        $number = $provider->between(1, 5);
+        $number = $provider->between(1, 4);
         $this->assertInternalType('int', $number);
-        $this->assertTrue($number >= 1 && $number <= 5);
+        $this->assertTrue($number >= 1 && $number <= 4);
+    }
+
+    public function testRandomNumber()
+    {
+        $provider = new Math();
+        $number = $provider->randomNumber(8);
+        $this->assertInternalType('int', $number);
+        $this->assertEquals(8, strlen((string)$number));
     }
 }
