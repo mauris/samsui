886408088bb8cf537237f823a7c80548e00f87c7
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-04 11:00:04 +0800

    added between() and randomNumber() to math provider

--- src/Samsui/Generator/Provider/Math.php
+++ src/Samsui/Generator/Provider/Math.php
@@ -4,4 +4,16 @@ namespace Samsui\Generator\Provider;
 
 class Math implements ProviderInterface
 {
+    public function between($lower = null, $upper = null)
+    {
+        return mt_rand($lower ?: 0, $upper ?: PHP_INT_MAX);
+    }
+
+    public function randomNumber($digits, $upper = null)
+    {
+        return $this->between(
+            pow(10, $digits - 1),
+            $upper ?: pow(10, $digits) - 1
+        );
+    }
 }
