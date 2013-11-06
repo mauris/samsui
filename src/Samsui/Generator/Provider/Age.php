c902f4ce98e3205249e62645b70016c071b98bab
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-06 22:19:35 +0800

    age picker implementation

--- src/Samsui/Generator/Provider/Age.php
+++ src/Samsui/Generator/Provider/Age.php
@@ -4,4 +4,28 @@ namespace Samsui\Generator\Provider;
 
 class Age extends BaseProvider
 {
+    public function groups(array $range)
+    {
+        $ranges = func_get_args();
+        $weight = array();
+        foreach ($ranges as $range) {
+            $weight[] = $range[1] - $range[0];
+        }
+        $index = $this->generator->math->randomWeightedArray($weight);
+        $range = $ranges[$index];
+        $age = $this->generator->math->between($range[0], $range[1]);
+        return $age;
+    }
+
+    public function between($lower, $upper)
+    {
+        $age = $this->generator->math->between($lower, $upper);
+        return $age;
+    }
+
+    public function pick($age)
+    {
+        $age = $this->generator->math->randomArrayValue(func_get_args());
+        return $age;
+    }
 }
