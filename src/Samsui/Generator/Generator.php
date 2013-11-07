40763f7dde79497671fa28a1f82c15cc758be5dc
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-07 18:15:20 +0800

    generator concrete implementation

--- src/Samsui/Generator/Generator.php
+++ src/Samsui/Generator/Generator.php
@@ -2,7 +2,21 @@
 
 namespace Samsui\Generator;
 
+use Samsui\Generator\Provider\ProviderInterface;
+
 class Generator implements GeneratorInterface
 {
+    protected $providers = array();
 
+    public function registerProvider($name, ProviderInterface $provider)
+    {
+        $this->providers[$name] = $provider;
+    }
+
+    public function __get($name)
+    {
+        if (isset($this->providers[$name])) {
+            return $this->providers[$name];
+        }
+    }
 }
