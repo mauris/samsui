8c44acf5a25bcf0206525e228b3f712509e303e6
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-10 03:28:43 +0800

    gender provider implementation

---
+++ src/Samsui/Generator/Provider/Gender.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Samsui\Generator\Provider;
+
+class Gender extends BaseProvider
+{
+    public function pick($list = array('M', 'F'))
+    {
+        $gender = $this->generator->math->randomArrayValue($list);
+        return $gender;
+    }
+
+    public function birthRatio($list = array('M' => 51.45, 'F' => 48.54))
+    {
+        $gender = $this->generator->math->randomWeightedArray($list);
+        return $gender;
+    }
+}
