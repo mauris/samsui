4d6492e1890f791ea9dd88123607c562de10b033
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-10 21:19:06 +0800

    improved numeric accuracy of gps values

--- src/Samsui/Generator/Provider/GPS.php
+++ src/Samsui/Generator/Provider/GPS.php
@@ -6,20 +6,20 @@ class GPS extends BaseProvider
 {
     public function latitude()
     {
-        $latitude = $this->generator->math->between(-90000, 90000) / 1000;
+        $latitude = $this->generator->math->between(-90000000, 90000000) / 1000000;
         return $latitude;
     }
 
     public function longitude()
     {
-        $longitude = $this->generator->math->between(-180000, 180000) / 1000;
+        $longitude = $this->generator->math->between(-180000000, 180000000) / 1000000;
         return $longitude;
     }
 
     public function latlon()
     {
         return array(
-           'latitude' => $this->latitude(),
+            'latitude' => $this->latitude(),
             'longitude' => $this->longitude()
         );
     }
