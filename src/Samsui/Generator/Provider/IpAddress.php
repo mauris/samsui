33952759cf87b46b47d0acfbe656da76af6c062a
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-15 00:29:11 +0800

    added ipv6 implementation

--- src/Samsui/Generator/Provider/IpAddress.php
+++ src/Samsui/Generator/Provider/IpAddress.php
@@ -20,4 +20,33 @@ class IpAddress extends BaseProvider
         }
         return $ip1 . '.' . $ip2 . '.' . $ip3 . '.' . $ip4;
     }
+
+    public function v6($ip1 = null, $ip2 = null, $ip3 = null, $ip4 = null, $ip5 = null, $ip6 = null, $ip7 = null, $ip8 = null)
+    {
+        if ($ip1 === null) {
+            $ip1 = $this->generator->math->between(0, 65535);
+        }
+        if ($ip2 === null) {
+            $ip2 = $this->generator->math->between(0, 65535);
+        }
+        if ($ip3 === null) {
+            $ip3 = $this->generator->math->between(0, 65535);
+        }
+        if ($ip4 === null) {
+            $ip4 = $this->generator->math->between(0, 65535);
+        }
+        if ($ip5 === null) {
+            $ip5 = $this->generator->math->between(0, 65535);
+        }
+        if ($ip6 === null) {
+            $ip6 = $this->generator->math->between(0, 65535);
+        }
+        if ($ip7 === null) {
+            $ip7 = $this->generator->math->between(0, 65535);
+        }
+        if ($ip8 === null) {
+            $ip8 = $this->generator->math->between(0, 65535);
+        }
+        return $ip1 . ':' . $ip2 . ':' . $ip3 . ':' . $ip4 . ':' . $ip5 . ':' . $ip6 . ':' . $ip7 . ':' . $ip8;
+    }
 }
