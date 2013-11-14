995380e0d75a755d0e648371d04cc2d449363087
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-15 00:37:34 +0800

    usage of hex() in Math

--- src/Samsui/Generator/Provider/IpAddress.php
+++ src/Samsui/Generator/Provider/IpAddress.php
@@ -23,29 +23,30 @@ class IpAddress extends BaseProvider
 
     public function v6($ip1 = null, $ip2 = null, $ip3 = null, $ip4 = null, $ip5 = null, $ip6 = null, $ip7 = null, $ip8 = null)
     {
+        $math = $this->generator->math;
         if ($ip1 === null) {
-            $ip1 = $this->generator->math->between(0, 65535);
+            $ip1 = $math->hex(($math->between(0, 65535)), 4);
         }
         if ($ip2 === null) {
-            $ip2 = $this->generator->math->between(0, 65535);
+            $ip2 = $math->hex(($math->between(0, 65535)), 4);
         }
         if ($ip3 === null) {
-            $ip3 = $this->generator->math->between(0, 65535);
+            $ip3 = $math->hex(($math->between(0, 65535)), 4);
         }
         if ($ip4 === null) {
-            $ip4 = $this->generator->math->between(0, 65535);
+            $ip4 = $math->hex(($math->between(0, 65535)), 4);
         }
         if ($ip5 === null) {
-            $ip5 = $this->generator->math->between(0, 65535);
+            $ip5 = $math->hex(($math->between(0, 65535)), 4);
         }
         if ($ip6 === null) {
-            $ip6 = $this->generator->math->between(0, 65535);
+            $ip6 = $math->hex(($math->between(0, 65535)), 4);
         }
         if ($ip7 === null) {
-            $ip7 = $this->generator->math->between(0, 65535);
+            $ip7 = $math->hex(($math->between(0, 65535)), 4);
         }
         if ($ip8 === null) {
-            $ip8 = $this->generator->math->between(0, 65535);
+            $ip8 = $math->hex(($math->between(0, 65535)), 4);
         }
         return $ip1 . ':' . $ip2 . ':' . $ip3 . ':' . $ip4 . ':' . $ip5 . ':' . $ip6 . ':' . $ip7 . ':' . $ip8;
     }
