df9d0a29388e8017948a2d43ec47b5693b218682
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-21 00:06:33 +0800

    implementation of UrlTest

---
+++ test/Samsui/Generator/Provider/UrlTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Samsui\Generator\Provider;
+
+use \PHPUnit_Framework_TestCase;
+use Samsui\Generator\Generator;
+
+class UrlTest extends PHPUnit_Framework_TestCase
+{
+    protected $generator;
+
+    protected function setUp()
+    {
+        $this->generator = new Generator();
+        $this->generator->registerProvider('math', new Math($this->generator));
+    }
+
+    public function testTLD()
+    {
+        $provider = new Url($this->generator);
+        $tld = $provider->tld();
+        $this->assertInternalType('string', $tld);
+        $this->assertTrue(4 >= strlen($tld));
+    }
+}
