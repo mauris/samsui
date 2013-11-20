80af2951f11692aeed8775858ffbab5f93ce6916
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-21 00:03:54 +0800

    implementation of random TLD based on the usage stats

--- src/Samsui/Generator/Provider/Url.php
+++ src/Samsui/Generator/Provider/Url.php
@@ -4,4 +4,10 @@ namespace Samsui\Generator\Provider;
 
 class Url extends BaseProvider
 {
+    public function tld()
+    {
+        $data = json_decode(file_get_contents(__DIR__ . '/Resource/tld.json'), true);
+        $tld = $this->generator->math->randomWeightedArray($data);
+        return $tld;
+    }
 }
