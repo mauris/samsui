7eb446f7ba5bcb82d664aba93143d12d968eb29c
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-21 18:39:58 +0800

    email random domain implementation

---
+++ src/Samsui/Generator/Provider/Email.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Samsui\Generator\Provider;
+
+class Email extends BaseProvider
+{
+    public function domain()
+    {
+        $data = json_decode(file_get_contents(__DIR__ . '/Resource/domain-names.json'), true);
+        $name = $this->generator->math->randomArrayValue($data['email']);
+        return $name;
+    }
+}
