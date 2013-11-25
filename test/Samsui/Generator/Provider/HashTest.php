897aa0ab8ad113e626851f7c69639c8bbb8bb74f
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-26 00:17:19 +0800

    implemented hash test

---
+++ test/Samsui/Generator/Provider/HashTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Samsui\Generator\Provider;
+
+use \PHPUnit_Framework_TestCase;
+use Samsui\Generator\Generator;
+
+class HashTest extends PHPUnit_Framework_TestCase
+{
+    protected $generator;
+
+    protected function setUp()
+    {
+        $this->generator = new Generator();
+        $this->generator->registerProvider('string', new String($this->generator));
+        $this->generator->registerProvider('math', new Math($this->generator));
+    }
+
+    public function testHash()
+    {
+        $provider = new Hash($this->generator);
+        $this->assertRegExp('/^[a-f0-9]{40}$/', $provider->hash('sha1'));
+    }
+}
