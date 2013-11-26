92265579103e784730287bfa3082752c49886bd9
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-27 01:31:25 +0800

    added tests for email

---
+++ test/Samsui/Generator/Provider/EmailTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Samsui\Generator\Provider;
+
+use \PHPUnit_Framework_TestCase;
+use Samsui\Generator\Generator;
+
+class EmailTest extends PHPUnit_Framework_TestCase
+{
+    protected $generator;
+
+    protected function setUp()
+    {
+        $this->generator = new Generator();
+        $this->generator->registerProvider('math', new Math($this->generator));
+    }
+
+    public function testDomain()
+    {
+        $provider = new Email($this->generator);
+        $this->assertRegExp('/^[a-z]+\.[a-z]+$/i', $provider->domain());
+    }
+}
