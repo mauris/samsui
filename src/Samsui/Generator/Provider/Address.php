6d9ab0a7f7284f29426ba1d5efd115c0af202438
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-28 23:38:36 +0800

    updated code and added postal generation

--- src/Samsui/Generator/Provider/Address.php
+++ src/Samsui/Generator/Provider/Address.php
@@ -9,7 +9,18 @@ class Address extends BaseProvider
     public function random()
     {
         $resource = $this->generator->locale->fetch('address');
+        $lists = isset($resource['lists']) ? $resource['lists'] : array();
+        unset($resource['lists']);
         $loader = new ResourceLoader($this->generator);
-        return $loader->load($resource);
+        return $loader->load($resource, $lists);
+    }
+
+    public function postal()
+    {
+        $resource = $this->generator->locale->fetch('address.postal');
+        $lists = isset($resource['lists']) ? $resource['lists'] : array();
+        unset($resource['lists']);
+        $loader = new ResourceLoader($this->generator);
+        return $loader->load($resource, $lists);
     }
 }
