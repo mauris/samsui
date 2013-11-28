7c50b80aa123f6329d67a6448b495c3c463d491e
Sam-Mauris Yong <mauris@hotmail.sg>
2013-11-28 22:33:31 +0800

    updated locale class

--- src/Samsui/Generator/Provider/Locale.php
+++ src/Samsui/Generator/Provider/Locale.php
@@ -4,4 +4,23 @@ namespace Samsui\Generator\Provider;
 
 class Locale extends BaseProvider
 {
+    protected $locale;
+
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    public function fetch($name)
+    {
+        $file = __DIR__ . '/Resource/' . $this->locale . '/' . $name . '.json';
+        if (is_file($file)) {
+            return json_decode(file_get_contents($file), true);
+        }
+    }
 }
