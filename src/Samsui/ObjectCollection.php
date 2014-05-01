317b241fba795c23ba86c04d31b4e7fab50ebe4f
Sam-Mauris Yong <mauris@hotmail.sg>
2014-05-01 23:05:29 +0800

    added incode docs for objectcollection

--- src/Samsui/ObjectCollection.php
+++ src/Samsui/ObjectCollection.php
@@ -5,7 +5,9 @@ class ObjectCollection
     protected $items = array();
 
     /**
-     * @param string $name
+     * Set an object to its corresponding name in the collection
+     * @param string $name The name of the object to set
+     * @param mixed $object The object to store in the collection
      */
     public function set($name, $object)
     {
@@ -13,7 +15,9 @@ class ObjectCollection
     }
 
     /**
-     * @param string $name
+     * Retrieve an object stored in the collection
+     * @param string $name Name of the object to retrieve
+     * @return mixed Returns the object in the collection.
      */
     public function get($name)
     {
