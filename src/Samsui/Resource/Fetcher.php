ad6098e581a74ec8426d1391e45c255ba66f49fd
Sam-Mauris Yong <mauris@hotmail.sg>
2014-05-04 00:03:38 +0800

    added implementation for fetch

--- src/Samsui/Resource/Fetcher.php
+++ src/Samsui/Resource/Fetcher.php
@@ -2,5 +2,40 @@
 
 class Fetcher
 {
-    
+    private $resourceDir;
+
+    public function __construct()
+    {
+        $this->resourceDir = realpath(__DIR__ . '/../../../data');
+    }
+
+    public function fetch($path)
+    {
+        $parts = explode('.', $path);
+        $filename = $this->resourceDir . '/' . array_shift($parts);
+        while (is_dir($filename)) {
+            $filename .= '/' . array_shift($parts);
+        }
+        $filename .= '.json';
+        if (is_file($filename)) {
+            $data = json_decode(file_get_contents($filename), true);
+            $lists = isset($data['lists']) ? $data['lists'] : array();
+            if (count($parts) == 2 && $parts[0] == 'lists') {
+                $data = $lists[$parts[1]];
+            } else {
+                foreach ($parts as $part) {
+                    if (isset($data['parts']) && isset($data['parts'][$part])) {
+                        $data = $data['parts'][$part];
+                    } else {
+                        $data = null;
+                        break;
+                    }
+                }
+                if (is_array($data)) {
+                    $data['lists'] = $lists;
+                }
+            }
+            return $data;
+        }
+    }
 }
