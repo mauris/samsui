d9bf30d456caca20fa9c1851f9d0b7d868bee70b
Sam-Mauris Yong <mauris@hotmail.sg>
2014-08-20 00:12:19 +0800

    added more test for Color

--- test/Samsui/Provider/ColorTest.php
+++ test/Samsui/Provider/ColorTest.php
@@ -23,4 +23,39 @@ class ColorTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($color[1] >= 0 && $color[1] <= 255);
         $this->assertTrue($color[2] >= 0 && $color[2] <= 255);
     }
+
+    public function testRandomRGB()
+    {
+        $provider = new Color($this->generator);
+        $color = $provider->random(Color::RGB);
+        $this->assertInternalType('array', $color);
+        $this->assertCount(3, $color);
+        $this->assertTrue($color[0] >= 0 && $color[0] <= 255);
+        $this->assertTrue($color[1] >= 0 && $color[1] <= 255);
+        $this->assertTrue($color[2] >= 0 && $color[2] <= 255);
+    }
+
+    public function testRandomRGBA()
+    {
+        $provider = new Color($this->generator);
+        $color = $provider->random(Color::RGBA);
+        $this->assertInternalType('array', $color);
+        $this->assertCount(4, $color);
+        $this->assertTrue($color[0] >= 0 && $color[0] <= 255);
+        $this->assertTrue($color[1] >= 0 && $color[1] <= 255);
+        $this->assertTrue($color[2] >= 0 && $color[2] <= 255);
+        $this->assertTrue($color[3] >= 0 && $color[3] <= 255);
+    }
+
+    public function testRandomCMYK()
+    {
+        $provider = new Color($this->generator);
+        $color = $provider->random(Color::CMYK);
+        $this->assertInternalType('array', $color);
+        $this->assertCount(4, $color);
+        $this->assertTrue($color[0] >= 0 && $color[0] <= 100);
+        $this->assertTrue($color[1] >= 0 && $color[1] <= 100);
+        $this->assertTrue($color[2] >= 0 && $color[2] <= 100);
+        $this->assertTrue($color[3] >= 0 && $color[3] <= 100);
+    }
 }
